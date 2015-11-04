0e7b38f896b9a7459ba4d7327d024335a64114ee
Yauhen Yakimovich <eugney.yakimovitch@gmail.com>
2015-11-04 17:56:02 +0100

    Simulating error in the job on termination start.

--- resubmittable.py
+++ resubmittable.py
@@ -2,19 +2,26 @@
 # stdlib imports
 import sys
 import time
-import gc3libs
 import mylog
+import logging
+import gc3libs
+from gc3libs.workflow import StopOnError
 
 
+logger = logging.getLogger(__name__)
 mylog.setup_logging('console')
 # gc3libs.configure_logger(logging.DEBUG, "gdemo")
 
 
-class CommandApp(gc3libs.Application):
+class CommandApp(gc3libs.Application, StopOnError):
 
     def __init__(self, args):
+        if isinstance(args, basestring):
+            args = [args]
         assert type(args) == list
-        if args[0].startswith
+        # No full path, then assume it's a bash command
+        if not args[0].startswith('/'):
+            args = ['/bin/bash'] + args
         gc3libs.Application.__init__(
             self,
             # the following arguments are mandatory:
@@ -25,27 +32,25 @@ class CommandApp(gc3libs.Application):
             # the rest is optional and has reasonable defaults:
             stdout="stdout.txt",)
 
-# Create an instance of GdemoSimpleApp
-app = GdemoSimpleApp()
+    def terminated(self):
+        logger.info('program was terminated')
+        print self.execution.state
+        print self.execution.returncode
+        self.execution.returncode = 1
 
+
+app = CommandApp('/bin/hostname')
 engine = gc3libs.create_engine()
 engine.add(app)
 
-# in case you want to select a specific resource, call
-# `Engine.select_resource(<resource_name>)`
-if len(sys.argv)>1:
+if len(sys.argv) > 1:
     engine.select_resource(sys.argv[1])
 
-# Periodically check the status of your application.
+
 while app.execution.state != gc3libs.Run.State.TERMINATED:
     print "Job in status %s " % app.execution.state
-    # `Engine.progress()` will do the GC3Pie magic:
-    # submit new jobs, update status of submitted jobs, get
-    # results of terminating jobs etc...
     engine.progress()
-
-    # Wait a few seconds...
     time.sleep(1)
 
-print "Job is now terminated."
-print "The output of the application is in `%s`." %  app.output_dir
+print("Job is now terminated.")
+print("The output of the application is in `%s`." % app.output_dir)
